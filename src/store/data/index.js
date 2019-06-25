225ee32d36af5cdee5564c895df1aa9aa380a5a0
Fernando Camargo <fernando.camargo@sellics.com>
2019-06-25 15:53:01 +0200

    Testing...

--- src/store/data/index.js
+++ src/store/data/index.js
@@ -22,7 +22,7 @@ export default ({ signature, setInitialState, reconcileState }) => {
       },
       getReducers({ format: setInitialState })
     ),
-    composeWithDevTools()
+    composeWithDevTools({ name: 'Testing...' })
   );
   const persistor = persistStore(store);
 
